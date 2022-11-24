62a9abf60b142e9cb6e5a09abe154d96189d0077
Morgan Elise Meadows <morganelise@Morgans-MBP-3.attlocal.net>
2022-11-24 02:08:15 -0600

    working on login

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request, json
 from flask_cors import CORS
-
 from functools import wraps
-import uuid
-import jwt
-import datetime
 
 from service.group_service import Group_Service
 from service.user_service import User_Service
@@ -15,41 +11,75 @@ load_dotenv()
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})
-url = os.getenv("DATABASE_URL")
+
+database_url = os.getenv("DATABASE_URL")
 api_key = os.getenv("API_KEY")
 
-group_service = Group_Service(url)
-user_service = User_Service(url)
+group_service = Group_Service(database_url, api_key)
+user_service = User_Service(database_url, api_key)
+
+def token_required(f):
+  @wraps(f)
+  def decorator(*args, **kwargs):
+    token = None
+    if 'Authorization' in request.headers:
+      token = request.headers['Authorization']
+
+    if not token:
+      return {'message': 'a valid token is missing'}, 401
+    try:
+      data = user_service.decode_token(token)
+      current_user = user_service.get_user_by_id(data['id'])
+    except:
+      return {'message': 'token is invalid'}, 401
+
+    return f(*args, **kwargs)
+  return decorator
+
+
 
 @app.get("/")
 def readyness():
-    return "Send it"
+  return "Send it"
 
 @app.post("/user/create")
 def create_user():
-    user = request.get_json()
-    return user_service.create(user)
+  user = request.get_json()
+  return user_service.create(user)
 
 @app.delete("/user/delete/<int:id>")
 def delete_user(id):
-    return user_service.delete(id)
+  return user_service.delete(id)
 
 @app.post("/user/authenticate")
 def authenticate_user():
-    user = request.get_json()
-    return user_service.authenticate(user)
+  user = request.get_json()
+  return user_service.authenticate(user)
+
+@app.get("/user/<string:id>")
+def get_user_by_id(id):
+  return user_service.get_user_by_id(id)
+
+
 
 @app.post("/group/create")
+@token_required
 def create_group():
-    data = request.get_json()
-    name = data["name"]
-    return group_service.create(name)
+  data = request.get_json()
+  name = data["name"]
+  return group_service.create(name)
 
 @app.delete("/group/delete/<int:id>")
+@token_required
 def delete_group(id):
-    return group_service.delete(id)
+  return group_service.delete(id)
+
+@app.get("/group")
+@token_required
+def get_all_groups():
+  return group_service.get_all_groups()
 
 @app.get("/group/get-group-counts")
 def get_group_counts():
-    groups = group_service.get_group_counts()
-    return json.dumps(groups)
+  groups = group_service.get_group_counts()
+  return json.dumps(groups)
--- service/user_service.py
+++ service/user_service.py
@@ -1,104 +1,128 @@
 import re
 import psycopg2
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask import jsonify
+
+import uuid
+import jwt
+import datetime
 
 CREATE_USERS_TABLE = ("""
-    CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, 
-        first_name TEXT, last_name TEXT, email TEXT, password TEXT);
+  CREATE TABLE IF NOT EXISTS users (id TEXT, first_name TEXT, 
+  last_name TEXT, email TEXT, password TEXT, token TEXT, role TEXT);
 """)
 
 class User_Service:
-    def __init__(self, url):
-        self.connection = psycopg2.connect(url)
-        with self.connection:
-            with self.connection.cursor() as cursor:
-                cursor.execute(CREATE_USERS_TABLE)
+  def __init__(self, database_url, api_key):
+    self.connection = psycopg2.connect(database_url)
+    self.api_key = api_key
+    with self.connection:
+      with self.connection.cursor() as cursor:
+        cursor.execute(CREATE_USERS_TABLE)
 
-    def create(self, user):
-        SELECT_USER_COUNT_BY_EMAIL = ("""
-            SELECT COUNT(*)
-            FROM users
-            WHERE email = %s;
-        """)
+  def create(self, user):
+    SELECT_USER_COUNT_BY_EMAIL = ("""
+      SELECT COUNT(*)
+      FROM users
+      WHERE email = %s;
+    """)
 
-        INSERT_USER = ("""
-            INSERT INTO users (first_name, last_name, email, password) 
-            VALUES (%s, %s, %s, %s) RETURNING id;
-        """)
+    INSERT_USER = ("""
+      INSERT INTO users (id, first_name, last_name, email, password, role) 
+      VALUES (%s, %s, %s, %s, %s, 'user') RETURNING id;
+    """)
 
-        print(user)
-        try:
-            first_name = user['first_name']
-            last_name = user['last_name']
-            email = user['email']
-            password = user['password']
-        except KeyError:
-            return {"message": "Bad request."}, 400
+    try:
+      user_id = str(uuid.uuid4())
+      first_name = user['first_name']
+      last_name = user['last_name']
+      email = user['email']
+      password = user['password']
+    except KeyError:
+      return {"message": "Bad request."}, 400
 
-        weaknesses = self.check_password_strength(password)
-        if weaknesses != '':
-            return {"message": weaknesses}, 400
+    weaknesses = self.check_password_strength(password)
+    if weaknesses != '':
+      return {"message": weaknesses}, 400
 
-        password_hash = generate_password_hash(password, method='sha256')
+    password_hash = generate_password_hash(password, method='sha256')
 
-        with self.connection:
-            with self.connection.cursor() as cursor:
-                cursor.execute(CREATE_USERS_TABLE)
-                cursor.execute(SELECT_USER_COUNT_BY_EMAIL, (email,))
-                existing_user_count = cursor.fetchone()[0]
-                if existing_user_count > 0:
-                    return {"message": f"User {email} already exists."}, 403
-                cursor.execute(INSERT_USER, (first_name, last_name, email, password_hash))
-                user_id = cursor.fetchone()[0]
-        return {"id": user_id, "message": f"User {email} added."}, 201
-    
-    def delete(self, id):
-        DELETE_USER = ("""
-            DELETE FROM users where id = %s;
-        """)
+    with self.connection:
+      with self.connection.cursor() as cursor:
+        cursor.execute(CREATE_USERS_TABLE)
+        cursor.execute(SELECT_USER_COUNT_BY_EMAIL, (email,))
+        existing_user_count = cursor.fetchone()[0]
+        if existing_user_count > 0:
+          return {"message": f"User {email} already exists."}, 403
+        cursor.execute(INSERT_USER, (user_id, first_name, last_name, email, password_hash))
+        user_id = cursor.fetchone()[0]
+    return {"id": user_id, "message": f"User {email} added."}, 201
+  
+  def delete(self, id):
+    DELETE_USER = ("""
+      DELETE FROM users where id = %s;
+    """)
 
-        with self.connection:
-            with self.connection.cursor() as cursor:
-                cursor.execute(CREATE_USERS_TABLE)
-                cursor.execute(DELETE_USER, (id,))
+    with self.connection:
+      with self.connection.cursor() as cursor:
+        cursor.execute(CREATE_USERS_TABLE)
+        cursor.execute(DELETE_USER, (id,))
 
-        return {"message": f"User {id} deleted."}, 204
+    return {"message": f"User {id} deleted."}, 204
 
-    def authenticate(self, user):
-        AUTHENTICATE_USER = ("""
-            SELECT password FROM users
-            WHERE email = %s
-        """)
+  def authenticate(self, user):
+    AUTHENTICATE_USER = ("""
+      SELECT id, role, password FROM users
+      WHERE email = %s
+    """)
 
+    try: 
+      email = user["email"]
+      password = user['password']
+    except KeyError:
+      return {"message": "Bad request."}, 400
+    with self.connection:
+      with self.connection.cursor() as cursor:
+        cursor.execute(CREATE_USERS_TABLE)
+        cursor.execute(AUTHENTICATE_USER, (email,))
         try: 
-            email = user["email"]
-            password = user['password']
-        except KeyError:
-            return {"message": "Bad request."}, 400
-        with self.connection:
-            with self.connection.cursor() as cursor:
-                cursor.execute(CREATE_USERS_TABLE)
-                cursor.execute(AUTHENTICATE_USER, (email,))
-                try: 
-                    password_hash = cursor.fetchone()[0]
-                except TypeError:
-                    return {"message": f"Email not found."}, 400
-                if not password_hash:
-                    print(password_hash)
-                if check_password_hash(password_hash, password):
-                    return {"message": f"Login Success!"}, 201
-                else:
-                    return {"message": f"Password incorrect."}, 404
+          record = cursor.fetchone()
+          user_id = record[0]
+          role = record[1]
+          password_hash = record[2]
+        except TypeError:
+          return {"message": f"Email not found."}, 400
+        if check_password_hash(password_hash, password):
+          token = jwt.encode({'id' : user_id, 'role': role,  'exp' : datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, self.api_key, "HS256")
+          return {"token": token, "message": f"Login Success!"}, 201
+        else:
+          return {"message": f"Password incorrect."}, 404
 
-    @staticmethod
-    def check_password_strength(password):
-        message = ''
-        if len(password) < 8:
-            message += 'Password needs at least 8 characters\n'
-        if len(re.findall('[a-z]', password)) == 0:
-            message += 'Password needs at least 1 lowercase letter\n'
-        if len(re.findall('[A-Z]', password)) == 0:
-            message += 'Password needs at least 1 uppercase letter\n'
-        if len(re.findall('[0-9]', password)) == 0:
-            message += 'Password needs at least 1 number\n'
-        return message
+  def decode_token(self, token):
+    return jwt.decode(token, self.api_key, algorithms=["HS256"])
+
+  def get_user_by_id(self, user_id):
+    GET_USER_BY_ID = ("""
+      SELECT * FROM users
+      WHERE id = %s;
+    """)
+
+    with self.connection:
+      with self.connection.cursor() as cursor:
+        cursor.execute(CREATE_USERS_TABLE)
+        cursor.execute(GET_USER_BY_ID, (user_id,))
+        user = cursor.fetchone()
+        return user
+
+  @staticmethod
+  def check_password_strength(password):
+    message = ''
+    if len(password) < 8:
+      message += 'Password needs at least 8 characters\n'
+    if len(re.findall('[a-z]', password)) == 0:
+      message += 'Password needs at least 1 lowercase letter\n'
+    if len(re.findall('[A-Z]', password)) == 0:
+      message += 'Password needs at least 1 uppercase letter\n'
+    if len(re.findall('[0-9]', password)) == 0:
+      message += 'Password needs at least 1 number\n'
+    return message
